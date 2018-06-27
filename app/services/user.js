3678f5a75f1b9f3d6d77f5d5b26522220e2d57c3
usbpendrive <ferry.fernando@gmail.com>
2018-06-27 16:12:27 +0700

    User Service.

---
+++ app/services/user.js
@@ -0,0 +1,43 @@
+'use strict';
+
+class UserService {
+    constructor(log, mongoose, httpStatus, errs) {
+        this.log = log;
+        this.mongoose = mongoose;
+        this.httpStatus = httpStatus;
+        this.errs = errs;
+    }
+
+    async createUser(body) {
+        const Users = this.mongoose.model('Users');
+        const {username} = body;
+        const user = await Users.findOne({username});
+
+        if (user) {
+            const err = new this.errs.InvalidArgumentError('User with username already exists');
+            return err;
+        }
+
+        let newUser = new Users(body);
+        newUser.birthdate = new Date(body.birthdate);
+        newUser = await newUser.save();
+
+        this.log.info('User created successfully');
+        return newUser;
+    }
+
+    async getUser(username) {
+        const Users = this.mongoose.model('Users');
+        const user = await Users.findOne({username});
+
+        if (!user) {
+            const err = new this.errs.NotFoundError(`User with username - ${username} does not exists`);
+            return err;
+        }
+
+        this.log.info('User fetched successfully');
+        return user;
+    }
+}
+
+module.exports = UserService;
